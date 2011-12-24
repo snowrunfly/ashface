ed7d7013536c3f5b69e2267b554798a6666950c7
snowrunfly <snowrunfly@gmail.com>
2011-12-25 01:34:03 +0800

    Signed-off-by: snowrunfly <snowrunfly@gmail.com>

---
+++ ASH_DAO/config.php
@@ -0,0 +1,18 @@
+<?php 
+define('ASH_DBHOST', 'localhost');
+define('ASH_DBUSER', 'root');
+define('ASH_DBPW', 'root');
+define('ASH_DBNAME', 'ash_sns');
+define('ASH_DBCHARSET', 'utf8');
+define('ASH_DBTABLEPRE', 'ash_');
+define('ASH_COOKIEPATH', '/');
+define('ASH_COOKIEDOMAIN', '');
+define('ASH_DBCONNECT', 0);
+define('ASH_CHARSET', 'utf-8');
+define('ASH_FOUNDERPW', 'd4f58e1e72875a1fb29bc0c0bd35ed29');
+define('ASH_FOUNDERSALT', '141424');
+define('ASH_KEY', '38e463eC0E8nak89a7b261368D9i305a2A1m4h850vdUa1c7aKfz5sbveK2Qbs9v');
+define('ASH_SITEID', '3Ke46Ue70W8can86ahbD6a3e849w3n5T2m1O4J8q0Ld7alcxasfg5xbPe02YbG9L');
+define('ASH_MYKEY', '3ze06Ver0A8iap8fa5b66P3P889d3h5e2z1k4C8600dIaKcoaxfA57b3eR2lbb9t');
+define('ASH_DEBUG', false);
+define('ASH_PPP', 20);
